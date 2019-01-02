c0a5526a3005633803146a74954a563425a12397
Calvinno <42626008+Calvinno@users.noreply.github.com>
2019-01-02 15:53:06 -0500

    Add files via upload

---
+++ GettingStarted/GettingStarted/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GettingStarted
+//
+//  Created by Calvin Cantin on 18-08-20.
+//  Copyright © 2018 Calvin Cantin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        var names = ["Tammy", "Cole"]
+        names.removeFirst()
+        names.removeFirst()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
